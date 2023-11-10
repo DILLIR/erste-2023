8eeedeaffd2dfb269b8418b90289cec48f192b03
Yurii Soma <yurii.soma@student.tuke.sk>
2023-11-10 22:35:07 +0100

    feat: loader

--- erste-2023/src/pages/shops.tsx
+++ erste-2023/src/pages/shops.tsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import { useShop } from "../hooks/ShopSlice";
 import Shop from "../components/shop";
-
+import { Loader } from "../components/loader";
 
 export function ShopList() {
   const shops = useShop();
@@ -13,18 +13,15 @@ export function ShopList() {
       justifyContent="center"
       direction="column"
     >
-      <Typography
-        variant="h1"
-        textAlign="center"
-        sx={{ color: "rgb(40,112,237)" }}
-      >
-        Erste 2023 - win project
-      </Typography>
-      <Stack direction={"column"} alignItems={"flex-start"} spacing={2}>
-        {shops?.map((shop) => {
-          return <Shop key={shop.id} id={shop.id} name={shop.title} />;
-        })}
-      </Stack>
+      {shops == null ? (
+        <Loader />
+      ) : (
+        <Stack direction={"column"} alignItems={"flex-start"} spacing={2}>
+          {shops?.map((shop) => {
+            return <Shop key={shop.id} id={shop.id} name={shop.title} />;
+          })}
+        </Stack>
+      )}
     </Stack>
   );
 }
