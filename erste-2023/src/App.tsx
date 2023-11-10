4a9b3ef0b04c3d73e614206bb65ae512f4606235
Yurii Soma <yurii.soma@student.tuke.sk>
2023-11-10 16:22:46 +0100

    init project

---
+++ erste-2023/src/App.tsx
@@ -0,0 +1,14 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+
+function App() {
+  return (
+    <Stack sx={{minHeight: "100vh"}} alignItems="center" justifyContent="center" direction="column">
+      <Typography variant="h1" textAlign="center" sx={{color: "rgb(40,112,237)"}}>
+        Erste 2023 - win project
+      </Typography>
+    </Stack>
+  );
+}
+
+export default App;
