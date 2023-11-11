e1f787f50722ccf94f376d1ff0370aa0d9be7c33
nazar <nazarii.korchevskyi@student.tuke.sk>
2023-11-11 01:54:16 +0100

    fixed some problems with npm

--- erste-2023/src/App.tsx
+++ erste-2023/src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-import Layout from "./components/Layout";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import Layout from './components/Layout';
 
 function App() {
-  return (
-    <React.StrictMode>
-        <RouterProvider router={router} ></RouterProvider>
-    </React.StrictMode>
-  );
+    return (
+        <React.StrictMode>
+            <RouterProvider router={router}></RouterProvider>
+        </React.StrictMode>
+    );
 }
 
 export default App;
